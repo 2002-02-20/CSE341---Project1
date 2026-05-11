bdd3dbc7b467ea7f09fc8b469aad4f8f90c8354a
dereklp <adhoc607gmail.com>
2026-05-11 00:10:57 -0500

    routes.rest

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 router.use('/users', require('./user'));
 
 module.exports = router;
